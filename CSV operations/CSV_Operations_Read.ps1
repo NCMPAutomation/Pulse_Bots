1d3bf59124f1d37e4dc637ed8acc9633f6868515
NCMPAutomation <netconncmp@netcon.in>
2024-10-24 15:18:41 +0530

    Update CSV_Operations_Read.ps1

--- CSV operations/CSV_Operations_Read.ps1
+++ CSV operations/CSV_Operations_Read.ps1
@@ -1,3 +1,19 @@
-Get-Process | Export-Csv -Path .\Processes.csv
-$P = Import-Csv -Path .\Processes.csv
-$P | Get-Member
+# Get the list of running processes
+$processes = Get-service
+
+# Define the path to the CSV file
+$csvPath = "C:\Users\UserID\Downloads\test.csv"
+
+# Export the process data to the CSV file
+$processes | Export-Csv -Path $csvPath -NoTypeInformation
+
+Write-Output "Process data exported to $csvPath"
+
+
+# Import the CSV data
+$processData = Import-Csv -Path $csvPath
+
+# Display the imported data
+$processData | Format-Table -AutoSize
+
+Write-Output "CSV data imported successfully from $csvPath"
