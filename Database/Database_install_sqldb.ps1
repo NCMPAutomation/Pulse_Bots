135f3f2cf93309edb923f7281d71dfdd69c0a2bf
NCMPAutomation <netconncmp@netcon.in>
2024-10-28 15:34:33 +0530

    Update Database_install_sqldb.ps1

--- Database/Database_install_sqldb.ps1
+++ Database/Database_install_sqldb.ps1
@@ -1,3 +1,11 @@
-#************** INSTALL SQL SERVER *******************************#
+# Define the URL for the SQL Server installer
+$installerUrl = "https://go.microsoft.com/fwlink/?linkid=866658"
 
-Install-Module -Name SQLServer
+# Define the path where the installer will be downloaded
+$installerPath = "C:\Temp\SQLServer2019.exe"
+
+# Download the SQL Server installer
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# Run the SQL Server installer
+Start-Process -FilePath $installerPath -ArgumentList "/Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQLENGINE /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT='NT AUTHORITY\SYSTEM' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS'" -Wait
