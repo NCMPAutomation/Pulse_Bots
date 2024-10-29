13e030a6761406327fedaa90ff0467981baeed64
NCMPAutomation <netconncmp@netcon.in>
2024-10-29 11:26:52 +0530

    Update ConversionUtilities_JsonToXml.ps1

--- Conversion Utilities/ConversionUtilities_JsonToXml.ps1
+++ Conversion Utilities/ConversionUtilities_JsonToXml.ps1
@@ -2,3 +2,6 @@
 $JsonObject = Get-Content -Path "path\to\your\file.json" | ConvertFrom-Json
 
 $XML = ConvertTo-Xml -As Stream -InputObject $JsonObject -Depth 3
+
+$XML | out-file -FilePath "path\to\your\file.xml" 
+
