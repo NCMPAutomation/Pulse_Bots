55ee7f6e803d1d332f20e5e916864f66ac652fb6
NCMPAutomation <netconncmp@netcon.in>
2024-11-05 11:34:01 +0530

    Update Active_directory_modify_user_properties.ps1

--- Active directory/Active_directory_modify_user_properties.ps1
+++ Active directory/Active_directory_modify_user_properties.ps1
@@ -4,7 +4,7 @@
                      $password1 = ''
                      $pass1= ConvertTo-SecureString $password1 -AsPlainText -Force
                      $azure_cred = New-Object System.Management.Automation.PsCredential( $username1, $pass1 )
-                     $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "3865b44b-651f-4df8-a0c8-2625494f6198" 
+                     $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "TENANT_ID" 
                      $set_company_name = Set-AzureADUser -ObjectId $user -CompanyName 'Netcon'
                      $set_country = Set-AzureADUser -ObjectId $user -Country 'Central India'
                      $set_dept = Set-AzureADUser -ObjectId $user -Department 'Test Delivery'
