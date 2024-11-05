384f06986ce7f41401663303eaf268c4b37ed95e
NCMPAutomation <netconncmp@netcon.in>
2024-11-05 11:33:40 +0530

    Update Active_directory_update_azureAD_group.ps1

--- Active directory/Active_directory_update_azureAD_group.ps1
+++ Active directory/Active_directory_update_azureAD_group.ps1
@@ -4,7 +4,7 @@
                      $password1 = 'password'
                      $pass1= ConvertTo-SecureString $password1 -AsPlainText -Force
                      $azure_cred = New-Object System.Management.Automation.PsCredential( $username1, $pass1 )
-                     $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "3865b44b-651f-4df8-a0c8-2625494f6198" 
+                     $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "TENANT_ID" 
                      $account_disable= Update-AzureADGroup -ObjectId $user -SecurityEnabled
                      
 
