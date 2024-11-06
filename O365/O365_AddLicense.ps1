436e6e3d0639b393e7510ad800337e025a71a403
NCMPAutomation <netconncmp@netcon.in>
2024-11-06 15:24:08 +0530

    Update O365_AddLicense.ps1

--- O365/O365_AddLicense.ps1
+++ O365/O365_AddLicense.ps1
@@ -1,4 +1,27 @@
-Import-Module Microsoft.Graph
-Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
-$e5Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E5'
-Set-MgUserLicense -UserId "belindan@litwareinc.com" -AddLicenses @{SkuId = $e5Sku.SkuId} -RemoveLicenses @()
+$user = ''
+$username1 = ''
+$password1 = ''
+$pass1= ConvertTo-SecureString $password1 -AsPlainText -Force
+$azure_cred = New-Object System.Management.Automation.PsCredential( $username1, $pass1 )
+ $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "" 
+
+# Define the user and the license
+$userPrincipalName = ""
+$licenseSkuId = "FLOW_FREE"  # Example for Power Automate Free license
+
+# Get the user object
+$user = Get-AzureADUser -ObjectId $userPrincipalName
+
+# Assign the license to the user
+$license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+$license.SkuId = (Get-AzureADSubscribedSku | Where-Object { $_.SkuPartNumber -eq $licenseSkuId }).SkuId
+$licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+$licenses.AddLicenses = $license
+
+Set-AzureADUserLicense -ObjectId $user.ObjectId -AssignedLicenses $licenses
+
+Write-Output "License added successfully to $userPrincipalName"
+
+
+
+
