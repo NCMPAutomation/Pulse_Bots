751ea6a11de4284f11040001574202f3c2e55815
NCMPAutomation <netconncmp@netcon.in>
2024-11-06 15:30:35 +0530

    Update O365_RemoveLicense.ps1

--- O365/O365_RemoveLicense.ps1
+++ O365/O365_RemoveLicense.ps1
@@ -1,4 +1,24 @@
-Import-Module Microsoft.Graph
-Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
-$e5Sku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E5'
-Set-MgUserLicense -UserId "belindan@litwareinc.com" -RemoveLicenses @($e5Sku.SkuId) -AddLicenses @{}
+$user = ''
+$username1 = ''
+$password1 = ''
+$pass1= ConvertTo-SecureString $password1 -AsPlainText -Force
+$azure_cred = New-Object System.Management.Automation.PsCredential( $username1, $pass1 )
+ $connect= Connect-AzureAD -Credential $azure_cred -WarningAction Ignore -Confirm:$false -Tenant "" 
+
+# Define the user and the license
+$userPrincipalName = ""
+# Define the user and the license
+$licenseSkuId = "FLOW_FREE"  # Example for Power Automate Free license
+
+# Get the user object
+$user = Get-AzureADUser -ObjectId $userPrincipalName
+
+# Remove the license from the user
+$license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+$license.SkuId = (Get-AzureADSubscribedSku | Where-Object { $_.SkuPartNumber -eq $licenseSkuId }).SkuId
+$licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+$licenses.RemoveLicenses = $license.SkuId
+
+Set-AzureADUserLicense -ObjectId $user.ObjectId -AssignedLicenses $licenses
+
+Write-Output "License removed successfully from $userPrincipalName"
