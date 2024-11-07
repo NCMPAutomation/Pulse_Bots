19907ec5497ffbee4e7e223c9c18a82bf35337ee
NCMPAutomation <netconncmp@netcon.in>
2024-11-07 16:50:42 +0530

    Update SharePoint_Site_CreateSite.ps1

--- SharePoint/SharePoint_Site_CreateSite.ps1
+++ SharePoint/SharePoint_Site_CreateSite.ps1
@@ -1,12 +1,26 @@
-Import-Module Pnp.Powershell
-$username = 'netdevopsid@netcon.in'
-$password = 'password'
-$pass= ConvertTo-SecureString $password -AsPlainText -Force
+# Load SharePoint CSOM Assemblies
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
 
-$credential = New-Object System.Management.Automation.PsCredential( $username, $pass )
-$siteUrl = "https://contoso.sharepoint.com/sites/Contoso"
-$groupName = "Team Site Members"
-$userEmail = "john.doe@contoso.com"
-Connect-PnPOnline -Url https://contoso-admin.sharepoint.com -Credential $credential
-New-PnPSite -Type TeamSite -Title "My Team Site" -Alias "myteamsite" -IsPublic
-Disconnect-PnPOnline
+# Define variables
+$siteUrl = ""
+$username = ""
+$password = ""
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword)
+
+# Connect to SharePoint Online
+$context = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
+$context.Credentials = $credentials
+
+# Create the site
+$webCreationInfo = New-Object Microsoft.SharePoint.Client.WebCreationInformation
+$webCreationInfo.Url = ""
+$webCreationInfo.Title = ""
+$webCreationInfo.WebTemplate = "STS#3"  # Template for a modern team site
+$webCreationInfo.UseSamePermissionsAsParentSite = $true
+
+$newWeb = $context.Web.Webs.Add($webCreationInfo)
+$context.ExecuteQuery()
+
+Write-Output "Site created successfully at $siteUrl"
