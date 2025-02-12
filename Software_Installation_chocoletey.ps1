2196c278d5de0d793e631dedb0187777edf27f57
NCMPAutomation <netconncmp@netcon.in>
2025-02-12 13:36:11 +0530

    Add files via upload

---
+++ Software_Installation_chocoletey.ps1
@@ -0,0 +1,12 @@
+# Check if Chocolatey is installed
+if (-not (Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+    Write-Output "Chocolatey is not installed. Installing Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+} else {
+    Write-Output "Chocolatey is already installed."
+}
+
+# Install Notepad++ using Chocolatey
+choco install notepadplusplus -y
