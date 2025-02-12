fc3eacc6a04ff270cdb09cebefe4d769cd6f4de6
NCMPAutomation <netconncmp@netcon.in>
2025-02-12 13:37:35 +0530

    Delete Software_Installation_MacOS.ps1

--- Software_Installation_MacOS.ps1
+++
@@ -1,8 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/netcon/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/netcon/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-#Define the path as per the requirement, set up enviroment variable
-brew install htop
-htop
-brew uninstall htop
