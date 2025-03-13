83f8203b5a28a35c3d1bc45ee7c9d745e90f6853
NCMPAutomation <netconncmp@netcon.in>
2025-03-13 11:58:21 +0530

    Add files via upload

---
+++ Azure/Azure – Terminate Virtual machine.ps1
@@ -0,0 +1,13 @@
+﻿# Connect to your Azure account
+Connect-AzAccount
+
+# Variables
+$resourceGroupName = "myResourceGroup"
+$vmName = "myWindowsVM"
+
+# Start the VM
+Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+# Check the status of the VM
+$vmStatus = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Status
+$vmStatus.Statuses
