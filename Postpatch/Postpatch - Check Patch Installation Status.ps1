64ddf31ecf9e951e2078683a8f95ce9bfb7cd94d
NCMPAutomation <netconncmp@netcon.in>
2025-04-02 15:50:12 +0530

    Update Postpatch - Check Patch Installation Status.ps1

--- Postpatch/Postpatch - Check Patch Installation Status.ps1
+++ Postpatch/Postpatch - Check Patch Installation Status.ps1
@@ -1,4 +1,4 @@
-﻿# Check the installation status of specific patches
+﻿# Check the installation status of specific patches applied
 $patches = @("KB123456", "KB789012")
 foreach ($patch in $patches) {
     $status = Get-HotFix | Where-Object { $_.Description -eq $patch }
