021700fa5cfbb5035872d509db489625076b6944
NCMPAutomation <netconncmp@netcon.in>
2025-04-03 10:35:48 +0530

    Update Email to DB - Archive Old Emails.ps1

--- Email to DB/Email to DB - Archive Old Emails.ps1
+++ Email to DB/Email to DB - Archive Old Emails.ps1
@@ -1,4 +1,4 @@
-﻿# Archive emails older than 30 days
+﻿# Archive the emails older than 30 days
 $ArchiveFolder = $Namespace.Folders["Archive"]
 foreach ($Email in $Emails) {
     if ((Get-Date) - $Email.ReceivedTime).Days -gt 30) {
